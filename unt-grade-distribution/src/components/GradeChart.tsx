f2ef3eab90eda2b83bb3aa8f181af62478931a71
smrtdylan <dylan.simeon6@gmail.com>
2026-02-28 15:34:29 -0600

    fix: remove gray cursor highlight on chart bar hover

--- unt-grade-distribution/src/components/GradeChart.tsx
+++ unt-grade-distribution/src/components/GradeChart.tsx
@@ -72,6 +72,7 @@ export default function GradeChart({
           stroke={chartColors.axisStroke}
         />
         <Tooltip
+          cursor={false}
           content={
             <CustomTooltip
               tooltipBg={chartColors.tooltipBg}
